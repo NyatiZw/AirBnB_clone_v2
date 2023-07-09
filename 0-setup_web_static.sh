394b56484f2b365d87f9b884aeec84481f68833d
NyatiZw <appsmaid19@gmail.com>
2023-07-09 11:32:12 -0700

    Prepare web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 #Script that sets up web servers for deployment of web_static
 
-sudo apt-get update
-sudo apt-get -y install nginx
+if ! command -v nginx &> /dev/null; then
+	sudo apt-get update
+	sudo apt-get -y install nginx
+fi
 
 # Create directories for web_static
 sudo mkdir -p /data/web_static/releases/test/
@@ -11,6 +13,9 @@ sudo mkdir -p /data/web_static/shared/
 # Create a fake HTML file
 echo "Hello, web_static!" | sudo tee /data/web_static/releases/test/index.html
 
+#Remove existing symbolic link if it exists
+sudo rm -rf /data/web_static/current
+
 # Create a symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
@@ -18,8 +23,7 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 #Update Nginx configuratipn to serve the content of /data/web_static/current/
-sudo sed -i 's/^\s*server_name\s.*;|server_name _;' /etc/nginx/sites_available/default
-sudo sed -i '/^\s*location \/ {/ a \\\tlocaton/hbnb_static/ {\n\t\talias /data/web_static/current/; \n\t}\n' /etc/nginx/sites-available/default
+sudo -i '/^\s*server_name\s/ a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/; \n\t}\n' /etc/nginx/sites-available/default
 
 # Restart Nginx
 sudo service nginx restart
