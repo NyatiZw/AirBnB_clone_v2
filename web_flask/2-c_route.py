87cbb64ccf9602fe31f0694b93be234853190962
NyatiZw <appsmaid19@gmail.com>
2023-07-22 05:06:18 +0000

    C is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,7 +18,11 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_with_text(text):
+    """ Function that displays text and
+        removes the "_" with a space
+    """
     return f"C {text.replace('_', ' ')}"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
