8f37dda129b2f2a4bd25653841f75de2a5d1abd0
NyatiZw <appsmaid19@gmail.com>
2023-07-22 21:01:52 +0000

    corrections

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -27,24 +27,24 @@ def c_with_text(text):
     return f"C {text}"
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_with_text(text):
+def python_with_text(text="is cool"):
     """ Function to display Python and the dynamic text """
     text = text.replace('_', ' ')
-    return f"Python {text}"
+    return "Python {}".format(text)
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_is_interger(n):
     """ Function to displlay number """
-    return f"{n} is a number"
+    return "{:d} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """ Function to display number template """
-    return render_template('5-number_template.html', number=n)
+    return render_template('5-number_template.html', n=n)
 
 
 if __name__ == "__main__":
